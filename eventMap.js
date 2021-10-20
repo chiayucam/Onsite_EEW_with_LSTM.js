1776660ebcc5b5b40664daabbfc186f781c7891a
chiayucam <48814609+chiayucam@users.noreply.github.com>
2021-10-20 17:26:57 +0800

    Update eventMap.js

--- eventMap.js
+++ eventMap.js
@@ -1,6 +1,6 @@
-function setMarkers(layerGroup, eventData, eventName) {
+function setStationMarkers(layerGroup, eventData, eventName) {
     eventData.forEach(function (datum) {
-        popupHTML = `
+        const popupHTML = `
         <p style="text-align:center;">
             <a href='waveformViewer.html?record=${eventName}${datum[2]}', style='font-size: 14px', Target='_blank'>
                 ${datum[2]}
@@ -13,6 +13,27 @@ function setMarkers(layerGroup, eventData, eventName) {
         layerGroup.addLayer(marker);
     })
 }
+
+function setEpicenterMarker(layerGroup, eventData, eventName) {
+    const epicenterIcon = L.icon({
+        iconUrl: 'star_icon.png',
+        iconSize: [30, 30],
+        iconAnchor: [15, 15]
+    });
+    const marker = L.marker(eventData["latlon"], {icon: epicenterIcon, zIndexOffset: 1000});
+    let eventDate, eventTime;
+    [eventDate, eventTime] = formatEventName(eventName).split(" ");
+    const popupHTML = `
+        <p style="text-align:center;">
+            ${eventDate}<br>
+            ${eventTime}<br>
+            M<sub>L</sub>: ${eventData["magnitude"]}
+        </p>
+        `;
+    marker.bindPopup(popupHTML);
+    layerGroup.addLayer(marker);
+}
+
 
 async function fetchJson(src) {
     const response = await fetch(src);
@@ -29,6 +50,30 @@ function formatEventName(eventName) {
     return `${year}/${month}/${date} ${hour}:${minute}:${second}`
 }
 
+function drawMap(eventList) {
+    let eventNames = Object.keys(eventList);
+    let baseMaps = {};
+    let layerGroup = [];
+    
+
+    eventNames.forEach(function (eventName) {
+        layerGroup[eventName] = L.layerGroup();
+
+        // draw station markers
+        src = `event_data/${eventName}.json`;
+        fetchJson(src).then(eventData => setStationMarkers(layerGroup[eventName], eventData, eventName));
+        baseMaps[formatEventName(eventName)] = layerGroup[eventName]
+
+        // draw epicenter marker
+        setEpicenterMarker(layerGroup[eventName], eventList[eventName], eventName)
+    });
+
+    L.control.layers(baseMaps).addTo(map)
+    baseMaps[formatEventName(eventNames[0])].addTo(map)
+};
+
+
+
 // load leaflet map
 let map = L.map('mapid');
 
@@ -41,19 +86,3 @@ map.addLayer(osm);
 // fetch event list json then feed event json data
 let src = "event_data/event_list.json"
 fetchJson(src).then(eventList => drawMap(eventList))
-
-
-function drawMap(eventList) {
-    let eventNames = Object.keys(eventList);
-    let baseMaps = {};
-    let layerGroup = [];
-    eventNames.forEach(function (eventName) {
-        src = `event_data/${eventName}.json`;
-        layerGroup[eventName] = L.layerGroup().addTo(map);
-        fetchJson(src).then(eventData => setMarkers(layerGroup[eventName], eventData, eventName));
-
-        baseMaps[formatEventName(eventName)] = layerGroup[eventName]
-    });
-
-    L.control.layers(baseMaps).addTo(map)
-};
